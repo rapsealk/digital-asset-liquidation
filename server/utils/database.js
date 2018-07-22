54ff1b063a47d5e67fee9f83ee64247a9f1b4d4a
rapsealk <piono623@naver.com>
2018-07-22 18:30:16 +0900

    move backend to https://github.com/rapsealk/digital-asset-node.git

--- server/utils/database.js
+++
@@ -1,6 +0,0 @@
-/*
-const mysqlPromise = require('promise-mysql');
-const mysqlPromiseConfiguration = require('./dbconfig.json');
-
-module.exports = mysqlPromise.createPool(mysqlPromiseConfiguration);
-*/
